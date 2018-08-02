3611b13d373b72b7c905a8fc02a3957df91047c2
Joel Smith <joelwsmith123@gmail.com>
2018-08-02 09:01:25 -0600

    add media querys

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var body = document.querySelector('body');
 var hamburgerMenu = document.querySelector('.hamburger-menu');
 var sidebar = document.querySelector('.sidebar');
 
+window.addEventListener('load', sidebarCollapse)
+window.addEventListener('resize', sidebarCollapse)
+
 hamburgerMenu.addEventListener('click', function() {
   if (sidebar.classList.contains('sidebar-hide')) {
     sidebar.classList.remove('sidebar-hide');
@@ -11,3 +14,14 @@ hamburgerMenu.addEventListener('click', function() {
     body.classList.add('body-sidebar-collapse');
   }
 })
+
+ function sidebarCollapse() {
+  if ( $(window).width() < 900) {      
+    sidebar.classList.add('sidebar-hide');
+    body.classList.add('body-sidebar-collapse');
+  } else if ( $(window).width() > 900) {
+    sidebar.classList.remove('sidebar-hide');
+    body.classList.remove('body-sidebar-collapse');
+  }
+}
+
